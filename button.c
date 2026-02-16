e33734db53a5a7e401e81e3cfaf246502abe5516
drac-0 <muhammadfaiz050607@gmail.com>
2026-02-16 22:21:45 +0700

    button test

---
+++ button.c
@@ -0,0 +1,18 @@
+#include <gtk/gtk.h>
+
+static void activate(GtkApplication *app){
+  GtkWidget *window = gtk_application_window_new(app);
+  GtkWidget *button = gtk_button_new_with_label("CLICK");
+  g_signal_connect_swapped(button, 
+                           "clicked" , 
+                           G_CALLBACK(gtk_window_close),
+                           window);
+  gtk_window_set_child(GTK_WINDOW(window), button);
+  gtk_window_present (GTK_WINDOW(window));
+} 
+
+int main(int argc, char *argv[]){
+  GtkApplication *app = gtk_application_new("TEST", G_APPLICATION_DEFAULT_FLAGS);
+  g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
+  return g_application_run(G_APPLICATION(app), argc, argv);
+}
